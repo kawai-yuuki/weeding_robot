325565b3f11934eee2ed1a78f0f2944ba74789b9
YukiKawai <117893322+kawai-yuuki@users.noreply.github.com>
2024-08-03 19:41:03 +0900

    Update camera_detection.py

--- src/weeding_robot/weeding_robot/camera_detection.py
+++ src/weeding_robot/weeding_robot/camera_detection.py
@@ -9,7 +9,7 @@ class CameraDetectionNode(Node):
         super().__init__("camera_detection")
         self.detection_result = False
         self.rf = RoboflowOak(model="weeds-tuhor", confidence=0.05, overlap=0.5, 
-                              version="1", api_key="SgN7kxjAfZ7qKQsNvFud", rgb=True, 
+                              version="1", api_key="", rgb=True, 
                               depth=False, device=None, blocking=True)
         self.presense_of_weeds_publisher_ = self.create_publisher(
             Bool, "presense_of_weeds", 10)
